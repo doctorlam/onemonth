43cde6de002c182cbb63507e1436b635d9f013c5
doctorlam <christopher.lam@unt.edu>
2014-11-03 11:35:26 -0600

    added devise for user management

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem "bootstrap-sass"
 gem 'sdoc', '~> 0.4.0'      
+gem 'devise'
 
 group :development, :test do 
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #note to set this to your actual host name
+  config.action_mailer.default_url_options = { host: 'client-proposal.herokuapp.com' }
+
 end
