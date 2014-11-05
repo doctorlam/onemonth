08fd17b6c59121e589f60142b35cdba820aaf876
doctorlam <christopher.lam@unt.edu>
2014-11-05 03:37:38 -0600

    Added dashboard that redirects to when logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+  	if user_signed_in?
+  		redirect_to dashboard_path
+  	end
   end
   
   def about
+  end
+
+  def dashboard
   end
 end
