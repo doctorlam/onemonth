0e85825b85db3e45b0e171f6a04a3454e166959f
doctorlam <chris.lam.unt@gmail.com>
2014-11-12 20:38:44 -0600

    adding migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ActiveRecord::Base
 
  	has_many :proposals
  	 belongs_to :admin
+ 	  # User::Roles
+  # The available roles
+  Roles = [ :admin , :default ]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+end
 
 end
