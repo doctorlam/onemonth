54d9a5f365506500fb454dc6e3d2cd3702fdc8f5
doctorlam <chris.lam.unt@gmail.com>
2014-11-17 13:24:09 -0600

    admin/non-admin functionality is working

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,25 +1,19 @@
 class ProposalsController < ApplicationController
-
+    load_and_authorize_resource :except => [:create]
     respond_to :html, :xml, :json
-    before_action :authenticate_user!, except: [:index, :show]  
-    before_action :correct_user, only: [:edit, :update, :destroy] 
-    before_action :set_proposal, only: [:show, :edit, :update, :destroy]
    
-
-  def index
+def index
 
     @proposals = Proposal.all
     respond_with(@proposals)
-   
+
   end
 
   def show
-    respond_with(@proposal)
+    
   end
 
   def new
-    @proposal = current_user.proposals.build
-    respond_with(@proposal)
     
   end
 
@@ -27,30 +21,25 @@ class ProposalsController < ApplicationController
   end
 
   def create
-    @proposal = current_user.proposals.build(proposal_params)
+ @proposal = current_user.proposals.build(proposal_params)
     @proposal.save
-    respond_with(@proposal)
+    respond_with(@proposal)    
   end
 
   def update
-    @proposal.update(proposal_params)
+   @proposal.update(proposal_params)
     respond_with(@proposal)
+      
   end
 
   def destroy
-    @proposal.destroy
+     @proposal.destroy
     respond_with(@proposal)
+      
   end
 
-  private
-    def set_proposal
-      @proposal = Proposal.find(params[:id])
-    end
-
-    def correct_user
-      @proposal = current_user.proposals.find_by(id: params[:id])
-      redirect_to proposals_path, notice: "Not authorized to edit this proposal" if @proposal.nil?
-    end
+  
+   
     def proposal_params
       
       params.require(:proposal).permit(:subject, :course_id, :semester_id, :time, :title, :abstract, :first_name, :last_name, :organization)
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,13 @@ class Ability
 
   def initialize(user)
     user ||=User.new
- if user.admin? 
-    can :manage, :all
-else
-    can :read, :all
-end
+  if user.nil?
+    can :read, Proposal
+  elsif user.admin?
+    can :manage, Proposal
+  else
+    can [:read, :create], Proposal
+    can [:update, :destroy], Proposal, :user_id => user.id
   end
+end
 end
