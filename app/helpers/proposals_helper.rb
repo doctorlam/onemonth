c7eb6cc039e9d75242aa0aa80b708865d18e804e
doctorlam <christopher.lam@unt.edu>
2014-11-28 10:50:21 -0600

    fixed pending to be auto

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -1,8 +1,8 @@
 module ProposalsHelper
 	def options_for_status
 		['Pending', 'Approved', 'Rejected', 'Completed']
-	end
-
+	
+end
 	def options_for_course
 		['TECM 3000', 'TECM 3100','TECM 3200','TECM 4100','TECM 4180','TECM 4190','TECM 4200','TECM 4250','TECM 4700', 'None']
 	end
